84a694914786c3be3250de6590564efe25d9225d
Onno van der Zee <hastalavista@dds.nl>
2024-10-24 22:29:17 +0200

    effective mocking, no call counting though

--- build/__tests__/stream-statics.test.js
+++ build/__tests__/stream-statics.test.js
@@ -1,11 +1,34 @@
 import assert from 'node:assert';
+import esmock from 'esmock';
 import { describe, it } from 'node:test';
 import { startServer } from '../stream-statics.js';
 import * as http from 'http';
+import * as https from 'https';
 describe('Serving http', async () => {
     it('should bring up an http server', async (t) => {
-        t.mock.method(http, 'createServer');
-        await startServer({ protocol: 'http' });
-        assert.strictEqual(http.createServer.mock.callCount(), 1);
+        const ststMocks = await esmock('../stream-statics.js', {
+            http: {
+                createServer: (cfg) => {
+                    return {};
+                }
+            }
+        });
+        const server = await startServer({ protocol: 'http' });
+        server.close();
+        assert(server instanceof http.Server, `Expected an http.Server instance, but found (other) ${server.constructor.name}`);
+    });
+});
+describe('Serving https', async () => {
+    it('should bring up an https server', async (t) => {
+        const ststMocks = await esmock('../stream-statics.js', {
+            http: {
+                createServer: (cfg) => {
+                    return {};
+                }
+            }
+        });
+        const server = await startServer({ protocol: 'https' });
+        server.close();
+        assert(server instanceof https.Server, `Expected an https.Server instance, but found (other) ${server.constructor.name}`);
     });
 });
--- src/__tests__/stream-statics.test.ts
+++ src/__tests__/stream-statics.test.ts
@@ -1,4 +1,5 @@
 import assert from 'node:assert'
+import esmock from 'esmock'
 import { describe, it, mock } from 'node:test'
 import { startServer } from '../stream-statics.js'
 import * as cfg from '../configuration.js'
@@ -10,10 +11,43 @@ import * as http2 from 'http2'
 describe('Serving http', async () => {
 
     it('should bring up an http server', async (t) => {
-        t.mock.method(http, 'createServer')
+        const ststMocks = await esmock('../stream-statics.js', {
+            http: {
+                 createServer: (cfg: any) => {
+                     return {} as http.Server
+                 }
+            }
+        })
 
-        await startServer({protocol: 'http'})
+        const server = await startServer({protocol: 'http'})
+        server.close()
 
-        assert.strictEqual(http.createServer.mock.callCount(), 1);
+        assert(
+            server instanceof http.Server,
+            `Expected an http.Server instance, but found (other) ${server.constructor.name}`
+        )
     })
+
+})
+
+describe('Serving https', async () => {
+
+    it('should bring up an https server', async (t) => {
+        const ststMocks = await esmock('../stream-statics.js', {
+            http: {
+                 createServer: (cfg: any) => {
+                     return {} as https.Server
+                 }
+            }
+        })
+
+        const server = await startServer({protocol: 'https'})
+        server.close()
+
+        assert(
+            server instanceof https.Server,
+            `Expected an https.Server instance, but found (other) ${server.constructor.name}`
+        )
+    })
+
 })
