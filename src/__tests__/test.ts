6bacc3b3b5ab328bef8e4b541ae2a4a9bfde06dc
Onno van der Zee <hastalavista@dds.nl>
2026-02-01 16:37:18 +0100

    notes

--- src/__tests__/test.ts
+++ src/__tests__/test.ts
@@ -1,2 +1,9 @@
 import('./configuration.test.js')
 import('./stream-statics.test.js')
+
+/* inspiration
+https://stackoverflow.com/questions/8021956/node-js-mock-http-request-and-response
+https://www.npmjs.com/package/node-mocks-http
+https://getsimple.works/how-to-mock-http-request-and-response
+https://www.npmjs.com/package/http-request-mock
+*/
